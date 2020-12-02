7a6736025d7215de937a4e4e732a322fc866a2db
ipmanlk <io@navinda.xyz>
2020-12-02 15:28:54 +0530

    add: crypto util

---
+++ Util/CryptographyUtil.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace sales_and_inventory.Util
+{
+    class CryptographyUtil
+    {
+        public static string GetHash(string input)
+        {
+            SHA256 hashAlgorithm = SHA256.Create();
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            var sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+        
+        public static Boolean VerifyHash(string input, string hash)
+        {
+            SHA256 hashAlgorithm = SHA256.Create();
+
+            // Hash the input.
+            var hashOfInput = GetHash(input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            return comparer.Compare(hashOfInput, hash) == 0;
+        }
+
+        public static byte[] EncryptByteArray(string key, byte[] input)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(Convert.ToBase64String(input));
+                        }
+
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+
+            return array;
+        }
+
+
+        public static byte[] DecryptByteArray(string key, byte[] encryptedInput)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = encryptedInput;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return System.Convert.FromBase64String(streamReader.ReadToEnd());
+                        }
+                    }
+                }
+            }
+            
+        }
+    }
+}
